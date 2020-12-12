6d7aa234a3a4f9b3786037ff503b5cd99aee9076
aitor <gmaitor@outlook.com>
2020-12-12 23:56:17 +0100

    text with image 50/50

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
 const reloadQuote = document.getElementById('reload') 
+const img_container = document.getElementById('img-container')
 const kanye_trump_img = document.getElementById('kanye-trump-img') 
-const kanye__talks = document.getElementById('kanye-talks') 
+const kanye_talks = document.getElementById('kanye-talks')
+const quoteText = document.getElementById('quote') 
 
 reloadQuote.addEventListener('click',()=>{
-    console.log(`reload`,);
     fadeOutImage(kanye_trump_img)
-    fadeInImage(kanye__talks)
+    fadeInImage(kanye_talks)
+    //img_container.classList.toggle('toTheLeft')
+    createQuoteSpeechBubble();
 })
 
 
 function fadeInImage(img){
     img.style.animation = `fadeImage linear 1s reverse forwards`
-    console.log(`saysImage`,img.style);
 }
 function fadeOutImage(img){
     img.style.animation = `fadeImage linear 1s forwards`
 
 }
+async function getQuote(){
+    const api = 'https://api.kanye.rest' // A free REST API for random Kanye West quotes (Kanye as a Service)
+    try{
+        const res = await fetch(api);
+        const jsonRes = await res.json()
+        const quote = jsonRes.quote
+        return quote;
+    }catch(e){
+        console.log(`QuetQuoteERR==>`,e);
+    }
+}
+
+async function createQuoteSpeechBubble(){
+    const quote = await getQuote();
+    let i = 1;
+    quoteText.classList.toggle('hidden')
+    // const writeQuoteInterval = setInterval(() => {        
+    //     const text = quote.slice(0, i);
+    //     quoteText.innerText = text;
+    //     i++;
+    //     checkClearInterval();
+
+    // }, 100);
+
+    function checkClearInterval(){
+        if(i>quote.length){
+            clearInterval(writeQuoteInterval);
+        }
+    }
+}
+
